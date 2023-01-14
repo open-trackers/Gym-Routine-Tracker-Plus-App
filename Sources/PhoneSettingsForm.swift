c7117900137109c38c29c69e1f5b506d5e7fa963
Reed Es <reedes@fastmail.com>
2023-01-14 12:13:12 -0700

    ongoing

--- Sources/PhoneSettingsForm.swift
+++ Sources/PhoneSettingsForm.swift
@@ -8,7 +8,6 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 //
 
-import CoreData
 import os
 import SwiftUI
 
@@ -16,54 +15,15 @@ import GroutLib
 import GroutUI
 
 struct PhoneSettingsForm: View {
-    @Environment(\.managedObjectContext) private var viewContext
     @EnvironmentObject private var router: MyRouter
-
-    // MARK: - Locals
-
-    @AppStorage(colorSchemeModeKey) var colorSchemeMode: ColorSchemeMode = .automatic
-    @AppStorage(colorSchemeModeKey) var exportFormat: ExportFormat = .CSV
-
-    private let logger = Logger(subsystem: Bundle.main.bundleIdentifier!,
-                                category: String(describing: PhoneSettingsForm.self))
-
-    @State private var showFileExport = false
-    @State private var zipDocument: ZipDocument?
-    @State private var zipFileName: String?
 
     // MARK: - Views
 
     var body: some View {
         SettingsForm {
-            Section {
-                Picker("Color", selection: $colorSchemeMode) {
-                    ForEach(ColorSchemeMode.allCases, id: \.self) { mode in
-                        Text(mode.description).tag(mode)
-                    }
-                }
-                .pickerStyle(SegmentedPickerStyle())
-            } header: {
-                Text("Color Scheme")
-                    .foregroundStyle(.tint)
-            }
+            ColorSettings()
 
-            Section {
-                Button(action: exportAction) {
-                    Text("Export Data")
-                }
-                Picker("", selection: $exportFormat) {
-                    ForEach(ExportFormat.allCases, id: \.self) { mode in
-                        Text(mode.defaultFileExtension.uppercased())
-                            .tag(mode)
-                    }
-                }
-                .pickerStyle(SegmentedPickerStyle())
-            } header: {
-                Text("Data Export")
-                    .foregroundStyle(.tint)
-            } footer: {
-                Text("Will export data to ZIP archive containing \(exportFormat.description) (\(exportFormat.rawValue)) files.")
-            }
+            ExportSettings()
 
             Button(action: {
                 router.path.append(MyRoutes.about)
@@ -71,60 +31,12 @@ struct PhoneSettingsForm: View {
                 Text("About \(appName)")
             }
         }
-        .fileExporter(isPresented: $showFileExport,
-                      document: zipDocument,
-                      contentType: .zip,
-                      defaultFilename: zipFileName) { result in
-            switch result {
-            case let .success(url):
-                logger.notice("\(#function): saved to \(url)")
-            case let .failure(error):
-                logger.error("\(#function): \(error.localizedDescription)")
-            }
-            zipDocument = nil
-            zipFileName = nil
-        }
     }
 
     // MARK: - Properties
 
     private var appName: String {
         Bundle.main.appName ?? "unknown"
-    }
-
-    // MARK: - Actions
-
-    private func exportAction() {
-        if let document = createZipDocument() {
-            zipDocument = document
-            zipFileName = generateTimestampFileName(prefix: "grt-", suffix: ".zip")
-            showFileExport = true
-        } else {
-            logger.error("Unable to generate zip document, so not exporting.")
-        }
-    }
-
-    // MARK: - Helpers
-
-    private func createZipDocument() -> ZipDocument? {
-        logger.notice("\(#function) ENTER")
-        do {
-            if let mainStore = PersistenceManager.getMainStore(viewContext),
-               let archiveStore = PersistenceManager.getArchiveStore(viewContext),
-               let data = try createZipArchive(viewContext,
-                                               mainStore: mainStore,
-                                               archiveStore: archiveStore,
-                                               format: exportFormat)
-            {
-                logger.notice("\(#function) EXIT (success)")
-                return ZipDocument(data: data)
-            }
-        } catch {
-            logger.error("\(#function): ERROR \(error.localizedDescription)")
-        }
-
-        logger.notice("\(#function) EXIT (failure)")
-        return nil
     }
 }
 
